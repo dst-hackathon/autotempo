cbf804c58f5a41421e47be4960a3e7ac2842ee7a
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-06 14:25:50 +0700

    Add mockito and wiremock for test

--- src/main/java/com/dstsystems/hackathon/autotempo/tempo/TempoSubmitter.java
+++ src/main/java/com/dstsystems/hackathon/autotempo/tempo/TempoSubmitter.java
@@ -7,13 +7,21 @@ import com.dstsystems.hackathon.autotempo.tempo.models.TempoWorklog;
 import com.dstsystems.hackathon.autotempo.tempo.models.TempoWorklogAttribute;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TempoSubmitter {
 
-    public static final String ACCOUNT_ATTRIBUTE = "_account_";
+    private static final String ACCOUNT_ATTRIBUTE = "_account_";
+    private static final String REST_WORKLOG_PATH = "/rest/tempo-timesheets/3/worklogs/";
 
     private TempoConfig tempoConfig;
 
@@ -21,7 +29,9 @@ public class TempoSubmitter {
         this.tempoConfig = tempoConfig;
     }
 
-    public void submitWorklog(WorklogModel worklogModel) {
+    public void submitWorklog(WorklogModel worklogModel) throws IOException {
+        String worklogJson = getWorklogJson(worklogModel);
+        sendHttpRequest(worklogJson);
     }
 
     protected String getWorklogJson(WorklogModel worklogModel) throws JsonProcessingException {
@@ -47,4 +57,32 @@ public class TempoSubmitter {
         return new ObjectMapper().writeValueAsString(tempoWorklog);
     }
 
+    private String sendHttpRequest(String content) throws IOException {
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+
+        try {
+            httpClient = HttpClientBuilder.create().build();
+            HttpPost httpPost = new HttpPost(tempoConfig.getUrl() + REST_WORKLOG_PATH);
+            httpPost.setHeader("Content-Type", "application/json");
+            httpPost.setHeader("Origin", tempoConfig.getUrl()); // Required for JIRA cloud instances
+            httpPost.setEntity(new StringEntity(content));
+
+            response = httpClient.execute(httpPost);
+
+            int httpStatus = response.getStatusLine().getStatusCode();
+            if (httpStatus != 200) {
+                throw new IOException("Got http response code " + httpStatus);
+            }
+
+            return IOUtils.toString(response.getEntity().getContent());
+        } finally {
+            if (httpClient != null) {
+                httpClient.close();
+            }
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
 }
