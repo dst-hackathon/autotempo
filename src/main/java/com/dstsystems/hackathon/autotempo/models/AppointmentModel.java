b86a722039313c296f0aadf235da947f13a3c2ff
kaotangP <kaotang@gmail.com>
2015-11-06 11:25:42 +0700

    add AppointmentModel.java.

--- src/main/java/com/dstsystems/hackathon/autotempo/models/AppointmentModel.java
+++ src/main/java/com/dstsystems/hackathon/autotempo/models/AppointmentModel.java
@@ -1,4 +1,47 @@
 package com.dstsystems.hackathon.autotempo.models;
 
+import java.util.Date;
+import java.util.List;
+
 public class AppointmentModel {
+
+    String subject;
+
+    private Date start;
+
+    private Date end;
+
+    List<String> categories;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
 }
