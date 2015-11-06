4176d83be77073515715b2aa16dd08e7ab9beccf
kaotangP <kaotang@gmail.com>
2015-11-06 14:15:39 +0700

    Add UserProfileModel.java.

---
+++ src/main/java/com/dstsystems/hackathon/autotempo/models/UserProfileModel.java
@@ -0,0 +1,37 @@
+package com.dstsystems.hackathon.autotempo.models;
+
+/**
+ * Created by user on 06/11/2015.
+ */
+public class UserProfileModel {
+
+    String uesrName;
+
+    String password;
+
+    String URL;
+
+    public String getUesrName() {
+        return uesrName;
+    }
+
+    public void setUesrName(String uesrName) {
+        this.uesrName = uesrName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
