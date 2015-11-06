532cf95082a1b72294413deef0a384f44cb1cd30
aozster <aozster@gmail.com>
2015-11-06 12:49:36 +0700

    Add javadoc comment

---
+++ src/test/java/com/dstsystems/hackathon/autotempo/models/SimpleCategoryMappingRuleTest.java
@@ -0,0 +1,59 @@
+package com.dstsystems.hackathon.autotempo.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class SimpleCategoryMappingRuleTest {
+    SimpleCategoryMappingRule rule;
+    @Before
+    public void setUp() throws Exception {
+        rule = new SimpleCategoryMappingRule();
+        rule.setComment("Test comment");
+        rule.setIssueKey("issue1");
+        rule.setAccountKey("account1");
+        rule.setCategory("training");
+    }
+
+    @Test
+    public void thatValidCategoryShouldMatch() throws Exception {
+        WorklogModel worklog = new WorklogModel();
+        AppointmentModel appointmentModel = new AppointmentModel();
+        appointmentModel.setCategories(Arrays.asList("training"));
+
+        assertTrue(rule.isMatch(appointmentModel));
+    }
+
+    @Test
+    public void thatInvalidCatagotyShouldNotMatch() throws Exception {
+        WorklogModel worklog = new WorklogModel();
+        AppointmentModel appointmentModel = new AppointmentModel();
+        appointmentModel.setCategories(Arrays.asList("meeting"));
+
+        assertFalse(rule.isMatch(appointmentModel));
+    }
+
+    @Test
+    public void thatAppointmentWithNoCategoryShouldNotMatch() throws Exception {
+        WorklogModel worklog = new WorklogModel();
+        AppointmentModel appointmentModel = new AppointmentModel();
+        appointmentModel.setCategories(Collections.<String>emptyList());
+
+        assertFalse(rule.isMatch(appointmentModel));
+    }
+
+    @Test
+    public void thatRuleShouldPopulateWorkingModel() throws Exception {
+        WorklogModel worklog = new WorklogModel();
+        AppointmentModel appointmentModel = new AppointmentModel();
+        appointmentModel.setCategories(Arrays.asList("training"));
+        rule.populateWorkingModel(worklog, appointmentModel);
+        assertEquals("account1", worklog.getAccountKey());
+        assertEquals("issue1", worklog.getIssueKey());
+        assertEquals("Test comment", worklog.getComment());
+    }
+}
