16db510adfc2c13eeb7a49213bb57aa6392c7530
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-06 11:11:42 +0700

    Add basic WorklogModel

--- src/main/java/com/dstsystems/hackathon/autotempo/models/WorklogModel.java
+++ src/main/java/com/dstsystems/hackathon/autotempo/models/WorklogModel.java
@@ -1,4 +1,53 @@
 package com.dstsystems.hackathon.autotempo.models;
 
+import java.util.Date;
+
 public class WorklogModel {
+
+    private String issueKey;
+    private String comment;
+    private String accountKey;
+    private long timeSpent;
+    private Date date;
+
+    public String getIssueKey() {
+        return issueKey;
+    }
+
+    public void setIssueKey(String issueKey) {
+        this.issueKey = issueKey;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getAccountKey() {
+        return accountKey;
+    }
+
+    public void setAccountKey(String accountKey) {
+        this.accountKey = accountKey;
+    }
+
+    public long getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(long timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
