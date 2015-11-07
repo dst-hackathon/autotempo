287bfed56cce2f8b6b93d5c239ae49803abf6815
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-07 15:28:55 +0700

    Refactor UserProfileServiceImpl

--- src/main/java/com/dstsystems/hackathon/autotempo/service/UserProfileServiceImpl.java
+++ src/main/java/com/dstsystems/hackathon/autotempo/service/UserProfileServiceImpl.java
@@ -3,76 +3,39 @@ package com.dstsystems.hackathon.autotempo.service;
 import com.dstsystems.hackathon.autotempo.models.ExchangeUserProfileModel;
 import com.dstsystems.hackathon.autotempo.models.TempoUserProfileModel;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.Properties;
 
-/**
- * Created by user on 06/11/2015.
- */
 public class UserProfileServiceImpl implements UserProfileService {
 
     @Override
     public ExchangeUserProfileModel getExchangeUserProfile(String pathFileName) throws IOException {
-        ExchangeUserProfileModel userProfile = new ExchangeUserProfileModel();
-
         Properties properties = readProperties(pathFileName);
-        Enumeration enuKeys = properties.keys();
 
-        while (enuKeys.hasMoreElements()) {
-            String key = (String) enuKeys.nextElement();
-            String value = properties.getProperty(key);
-            switch (key) {
-                case "exchange.userName":
-                    userProfile.setUsername(value);
-                    break;
-                case "exchange.password":
-                    userProfile.setPassword(value);
-                    break;
-                case "exchange.URL":
-                    userProfile.setURL(value);
-                    break;
-                default:
-                    break;
-            }
-        }
+        ExchangeUserProfileModel userProfile = new ExchangeUserProfileModel();
+        userProfile.setUsername(properties.getProperty("exchange.userName"));
+        userProfile.setPassword(properties.getProperty("exchange.password"));
+        userProfile.setURL(properties.getProperty("exchange.URL"));
 
         return userProfile;
     }
 
     @Override
     public TempoUserProfileModel getTempoUserProfile(String pathFileName) throws IOException {
-        TempoUserProfileModel userProfile = new TempoUserProfileModel();
-
         Properties properties = readProperties(pathFileName);
-        Enumeration enuKeys = properties.keys();
 
-        while (enuKeys.hasMoreElements()) {
-            String key = (String) enuKeys.nextElement();
-            String value = properties.getProperty(key);
-            switch (key) {
-                case "tempo.userName":
-                    userProfile.setUsername(value);
-                    break;
-                case "tempo.password":
-                    userProfile.setPassword(value);
-                    break;
-                case "tempo.URL":
-                    userProfile.setURL(value);
-                    break;
-                default:
-                    break;
-            }
-        }
+        TempoUserProfileModel userProfile = new TempoUserProfileModel();
+        userProfile.setUsername(properties.getProperty("tempo.userName"));
+        userProfile.setPassword(properties.getProperty("tempo.password"));
+        userProfile.setURL(properties.getProperty("tempo.URL"));
 
         return userProfile;
     }
 
     private Properties readProperties(String pathFileName) throws IOException {
         Properties properties = new Properties();
-        try (FileInputStream fileInput = new FileInputStream(pathFileName);) {
+        try (FileInputStream fileInput = new FileInputStream(pathFileName)) {
             properties.load(fileInput);
             return properties;
         }
