9e5c509d8b74d04277d995cd3ace074abdc3ca74
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-07 11:34:59 +0700

    Improve commandline parameter description

--- src/main/java/com/dstsystems/hackathon/autotempo/main/CommandLineArguments.java
+++ src/main/java/com/dstsystems/hackathon/autotempo/main/CommandLineArguments.java
@@ -13,7 +13,7 @@ public class CommandLineArguments {
     @Parameter(names = "-rule", description = "Path to rule configuration")
     private String rulePath = "rules.xml";
 
-    @Parameter(names = "-date", description = "Date to log (default: today's date)")
+    @Parameter(names = "-date", description = "Date to log in YYYY-mm-dd format")
     private Date date = Calendar.getInstance().getTime();
 
     @Parameter(names = "-json", description = "Use JSON file instead of connecting to exchange (for debugging)", hidden = true)
