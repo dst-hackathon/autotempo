4eb76b1b49e976b8c46a57597fd12b0976350b62
Tisana Roongsawang <tisana@gmail.com>
2019-06-06 22:15:04 +0700

    Vulnerability fix

--- src/test/java/com/dstsystems/hackathon/autotempo/utils/DateTestUtils.java
+++ src/test/java/com/dstsystems/hackathon/autotempo/utils/DateTestUtils.java
@@ -3,19 +3,20 @@ package com.dstsystems.hackathon.autotempo.utils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateTestUtils {
 
     public static Date buildDate(String date) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
         return sdf.parse(date);
     }
 
     public static Date buildDateTime(String dateTime) throws ParseException {
         try {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime);
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).parse(dateTime);
         } catch (ParseException e) {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateTime);
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US).parse(dateTime);
         }
     }
 
